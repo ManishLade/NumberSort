b51077e8be579a63ab3ad42d1bfcdff4fa510f81
ManishLade <39298040+ManishLade@users.noreply.github.com>
2022-09-25 04:26:35 +0530

    TEst

--- NumberSort/CsvHelper.cs
+++ NumberSort/CsvHelper.cs
@@ -10,34 +10,34 @@ namespace NumberSort
 {
     public class CsvHelperUtility
     {
-        
-        /// <summary>        
-        /// /// Gets the patient list from input CSV.        
-        /// </summary> 
-        /// <param name="inputFilePath">The input file path.</param>        
-        /// <returns></returns>       
-        public static IEnumerable<PhoneNumbers> GetDataFromInputCsv(string inputFilePath)        
-        {            
-            if (File.Exists(inputFilePath))            
-            {               
-                using (var reader = new StreamReader(inputFilePath))                
-                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))                
-                {                    
-                    try                    
-                    {                        
-                        var records = csv.GetRecords<PhoneNumbers>();                        
+        /// <summary>
+        /// /// Gets the patient list from input CSV.
+        /// </summary>
+        /// <param name="inputFilePath">The input file path.</param>
+        /// <returns></returns>
+        public static IEnumerable<PhoneNumbers> GetDataFromInputCsv(string inputFilePath)
+        {
+            if (File.Exists(inputFilePath))
+            {
+                using (var reader = new StreamReader(inputFilePath))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    try
+                    {
+                        var records = csv.GetRecords<PhoneNumbers>();
                         var result = records.ToList();
                         return result;
-                    }                    
-                    catch (Exception ex)                    
-                    {                        
-                        Console.WriteLine($"CSV file is not valid");                        
-                        return null;                    
-                    }                
-                }            
-            }            
-            Console.WriteLine($"CSV file does not exist on given path: {inputFilePath}");            
-            return null;        
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"CSV file is not valid");
+                        return null;
+                    }
+                }
+            }
+
+            Console.WriteLine($"CSV file does not exist on given path: {inputFilePath}");
+            return null;
         }
 
         public static void WriteOutPutCsv(string outputFilePath, int[] numbers)
